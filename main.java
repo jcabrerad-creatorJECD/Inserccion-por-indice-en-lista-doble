2d3b0c6970743a46cc2f100bb403882028158a6c
jcabrerad-creatorJECD <j.cabrerad@gmail.com>
2026-04-30 13:41:45 -0500

    Add TODOs for Node and DoublyLinkedList implementation
    Added TODO comments for implementing Node class and DoublyLinkedList with insertAtIndex method.

--- main.java
+++ main.java
@@ -1,1 +1,10 @@
+public class Main {
 
+    // TODO: Implementa la clase Node aquí
+
+    // TODO: Implementa la clase DoublyLinkedList con el método insertAtIndex aquí
+
+    public static void main(String[] args) {
+        // TODO: Crea una lista, inserta elementos usando insertAtIndex y muestra el resultado
+    }
+}
